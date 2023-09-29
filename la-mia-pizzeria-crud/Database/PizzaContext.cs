eb47b5a2bd1af2614508b84b659ca7d96fdc58a2
Blaser993 <corradodepinto.design@gmail.com>
2023-09-29 14:54:57 +0200

    database inizializzato

---
+++ la-mia-pizzeria-crud/Database/PizzaContext.cs
@@ -0,0 +1,16 @@
+﻿using la_mia_pizzeria_static.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace la_mia_pizzeria_static.Database
+{
+    public class PizzaContext : DbContext
+    {
+        public DbSet<Pizza> Pizze {  get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=MiaPizzeria;Integrated Security=True;TrustServerCertificate=True");
+
+        }
+    }
+}
