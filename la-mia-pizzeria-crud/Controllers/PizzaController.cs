eb9b8a2f61ac382a0386c04c53fed9ddf9194765
Blaser993 <corradodepinto.design@gmail.com>
2023-09-29 17:00:49 +0200

    stampo model pizza

---
+++ la-mia-pizzeria-crud/Controllers/PizzaController.cs
@@ -0,0 +1,20 @@
+﻿using la_mia_pizzeria_static.Database;
+using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            using(PizzaContext db = new PizzaContext())
+            {
+                List<Pizza> pizze = db.Pizze.ToList<Pizza>();
+
+                return View("Index", pizze);
+            }
+            
+        }
+    }
+}
--- la-mia-pizzeria-crud/Models/Pizza.cs
+++ la-mia-pizzeria-crud/Models/Pizza.cs
@@ -1,10 +1,19 @@
-﻿namespace la_mia_pizzeria_static.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace la_mia_pizzeria_static.Models
 {
     public class Pizza
     {
         public int Id { get; set; }
+
+        [MaxLength(100)]
         public string Name { get; set; }
+
+        [Column(TypeName = "text")]
         public string Description { get; set; }
+
+        [MaxLength(100)]
         public string Image { get; set; }
         public float Prize { get; set; }
 
