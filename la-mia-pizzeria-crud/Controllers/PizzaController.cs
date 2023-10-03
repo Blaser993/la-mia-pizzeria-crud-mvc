5c7afe3b248141fcd270208b95fb84867de45baf
Blaser993 <corradodepinto.design@gmail.com>
2023-10-03 12:37:09 +0200

    update

--- la-mia-pizzeria-crud/Controllers/PizzaController.cs
+++ la-mia-pizzeria-crud/Controllers/PizzaController.cs
@@ -61,5 +61,56 @@ namespace la_mia_pizzeria_static.Controllers
             }
             
         }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            using (PizzaContext db = new PizzaContext())
+            {
+                Pizza?pizzaToEdit = db.Pizze.Where(pizza => pizza.Id == id).FirstOrDefault();
+
+                if (pizzaToEdit == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return View(pizzaToEdit);
+                }
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(int id, Pizza oldPizza)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Update", oldPizza);
+            }
+
+            using (PizzaContext db = new PizzaContext())
+            {
+                Pizza pizzaToEdit = db.Pizze.Where(pizza => pizza.Id == id).FirstOrDefault();
+
+                if (pizzaToEdit != null)
+                {
+                    pizzaToEdit.Name = oldPizza.Name;
+                    pizzaToEdit.Description = oldPizza.Description;
+                    pizzaToEdit.Image = oldPizza.Image;
+                    pizzaToEdit.Prize = oldPizza.Prize;
+
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+
+                else
+                {
+                    return NotFound();
+                }
+            }
+        }
+
     }
 }
